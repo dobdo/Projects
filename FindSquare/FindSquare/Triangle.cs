f0449a8e709fccc699c235616534f4a46aa73b0c
dobdo <39757132+dobdo@users.noreply.github.com>
2018-11-25 19:40:24 +0300

    Create Triangle.cs

---
+++ FindSquare/FindSquare/Triangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSLib
+{
+    public class Triangle : IGetSquare
+    {
+        public readonly bool RightTriangle = false;
+        double SideA, SideB, SideC;
+
+        public Triangle(double SideA, double SideB, double SideC)
+        {
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.SideC = SideC;
+            RightTriangle = ItRightTriangle(SideA, SideB, SideC);
+
+        }
+
+        public double GetSquare()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+
+        bool ItRightTriangle(double SideA, double SideB, double SideC)
+        {
+            if ((Math.Pow(SideA, 2) + Math.Pow(SideB, 2) == Math.Pow(SideC, 2)) ||
+                (Math.Pow(SideA, 2) + Math.Pow(SideC, 2) == Math.Pow(SideB, 2)) ||
+                (Math.Pow(SideB, 2) + Math.Pow(SideC, 2) == Math.Pow(SideA, 2)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
