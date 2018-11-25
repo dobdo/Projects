043d2d949369bc81e7d60feffbc2f499f610756f
dobdo <39757132+dobdo@users.noreply.github.com>
2018-11-25 19:40:04 +0300

    Create Circle.cs

---
+++ FindSquare/FindSquare/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FindSLib
+{
+    public class Сircle : IGetSquare
+    {
+        const float Pi = 3.14F;
+        double Radius;
+
+        public Сircle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        public double GetSquare()
+        {
+            return Pi * (Math.Pow(Radius, 2));
+        }
+
+
+    }
+}
