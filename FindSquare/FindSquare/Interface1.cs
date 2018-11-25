d31e344b174b611fb1633411fb9127cd23dae163
dobdo <39757132+dobdo@users.noreply.github.com>
2018-11-25 19:44:42 +0300

    Create Interface1.cs

---
+++ FindSquare/FindSquare/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSLib
+{
+    public interface IGetSquare
+    {
+        double GetSquare();
+    }
+}
